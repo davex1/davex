73d2f143d39a004e6fdfd21baf823bd36c79371a
davex <davexdaw@gmail.com>
2015-03-04 09:46:47 +0100

    Change-Id: Ib03f7957faf86ae5ff9dfc0e9820a13ee598b28f Signed-off-by: davex <davexdaw@gmail.com>

--- davex/src/davex/Hola.java
+++ davex/src/davex/Hola.java
@@ -4,9 +4,11 @@ public class Hola {
 
 	public static void main(String[] args) {
 		
+		int x=7;
 		String s = "Hola mundo";
 		System.out.println(s);
 		System.out.println("Servidor");
+		
 	}
 	
 }
